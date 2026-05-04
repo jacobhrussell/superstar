2fda015ebf7c7fd79097f60b05750115d886e768
jacobhrussell <jacobhenryrussell@gmail.com>
2026-05-03 20:04:36 -0500

    adding dir flag to session new

--- cmd/session_new.go
+++ cmd/session_new.go
@@ -6,14 +6,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var sessionNewDir string
+
 var sessionNewCmd = &cobra.Command{
 	Use:   "new",
 	Short: "Create a new session",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("hello, superstar")
+		fmt.Println("dir:", sessionNewDir)
 	},
 }
 
 func init() {
+	sessionNewCmd.Flags().StringVar(&sessionNewDir, "dir", "", "directory to open in the session")
 	sessionCmd.AddCommand(sessionNewCmd)
 }
